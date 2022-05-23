4d18cdea786357c883cd8a51db86be23bc0476a5
Andzelika Botwina <Andzelika.botwina@gmail.com>
2022-05-23 20:06:25 +0200

    working on html file

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass')(require('sass'));
+
+gulp.task('sass', function() {
+  return gulp.src('./scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('default', function(){
+  return gulp.watch('./scss/**/*.scss', gulp.series('sass'));
+});
