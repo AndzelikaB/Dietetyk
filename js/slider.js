17d29a5189e6df397c3881320ecd44d99d0bc6b1
Andzelika Botwina <Andzelika.botwina@gmail.com>
2022-06-20 19:05:54 +0200

    js file

---
+++ js/slider.js
@@ -0,0 +1,9 @@
+        $('.slider').slick({
+            dots: true,
+            infinite: true,
+            speed: 2,
+            slidesToShow: 1,
+            adaptiveHeight: true,
+            fade: true,
+            cssEase: 'linear'
+        });
---
+++ js/sticky-navbar.js
@@ -0,0 +1,11 @@
+ // Sticky navbar
+ var navbar = document.getElementById("navbar");
+ var menu = document.getElementById("menu");
+
+ window.onscroll = function () {
+     if (window.pageYOffset >= menu.offsetTop) {
+         navbar.classList.add("sticky");
+     } else {
+         navbar.classList.remove("sticky");
+     }
+ }
