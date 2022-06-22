de60673ae8fc629593683dba26413a7f60ef0bfa
Andzelika Botwina <Andzelika.botwina@gmail.com>
2022-06-22 19:49:56 +0200

    working on paralax

--- js/paralax-effect.js
+++ js/paralax-effect.js
@@ -1,2 +1,13 @@
 var scene = document.getElementById('scene');
 var parallaxInstance = new Parallax(scene);
+
+window.addEventListener('scroll', function () {
+    const parallax = document.querySelector(".parallax");
+    let scroolPosition = window.pageYOffset;
+
+    parallax.style.transform = 'trnaslateY(' + scroolPosition * .5 + 'px)';
+
+
+
+
+})
