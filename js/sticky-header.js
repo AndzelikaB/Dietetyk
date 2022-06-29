2ab1e5f9ab7d7b43671ab1d360977d5915e3592c
Andzelika Botwina <Andzelika.botwina@gmail.com>
2022-06-29 22:53:49 +0200

    changes header styling

---
+++ js/sticky-header.js
@@ -0,0 +1,11 @@
+ // Sticky navbar
+ var header = document.getElementById("header");
+ var headerWrapper = document.getElementById("header-wrapper");
+
+ window.onscroll = function () {
+     if (window.pageYOffset >= headerWrapper.offsetHeight) {
+        header.classList.add("sticky");
+     } else {
+        header.classList.remove("sticky");
+     }
+ }
