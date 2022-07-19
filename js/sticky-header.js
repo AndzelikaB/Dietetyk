acd91d51e8f65c16c2df99626d8da74e187bcf56
Andzelika Botwina <Andzelika.botwina@gmail.com>
2022-07-19 22:29:55 +0200

    fixed header

--- js/sticky-header.js
+++ js/sticky-header.js
@@ -1,4 +1,9 @@
-var headerWrapper = document.getElementById("header-wrapper");
+var headerWrapper;
+var header;
+window.addEventListener('DOMContentLoaded',function () {
+  headerWrapper = document.getElementById("header-wrapper");
+  header = document.getElementById('site-header');
+});
 window.onscroll = function () {
    if (window.pageYOffset >= headerWrapper.offsetHeight) {
       header.classList.add("sticky");
